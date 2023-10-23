8aef6b158becc275015e3e5b47015a337e14c133
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-23 17:10:11 +0300

    create promise

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		let number = generateRandomNumber(1, 100);
+
+		if (number % 2 === 0) {
+			resolve(number);
+		} else {
+			reject(number);
+		}
+	}, 1000);
+});
+
 function generateRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
