a7e3ed028a08ea9ec22510807b47de33ca99c9c4
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-23 17:18:06 +0300

    handle the promise

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ let promise = new Promise((resolve, reject) => {
 	}, 3000);
 });
 
+promise
+	.then((number) => {
+		console.log(`Завершено успешно. Сгенерированное число — ${number}`);
+	})
+	.catch((number) => {
+		console.log(`Завершено с ошибкой. Сгенерированное число — ${number}`);
+	});
+
 function generateRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
